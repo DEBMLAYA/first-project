88338741943f64eeada447c6be04467971e31d5d
DEBMALYA <saudebmalya49@gmail.com>
2026-03-01 19:56:58 +0530

    dsa dequeue

---
+++ dequeue.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    
+}
+int dequeue()// check underflow...1st check
+{
+    int data,front , rear ;
+    if (front==-1)
+    {
+        printf("the queue is empty");
+        return 0;
+    }
+    
+    data=a[front]// deleat the data
+    if (front == rear)
+    {
+        front =rear -1;
+    }
+    else
+    {
+        front=front-1
+    }
+    return data;
+    
+}
