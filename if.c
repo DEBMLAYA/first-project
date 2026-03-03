90b670aab5ee30a6e6fb67468ea755765055073f
DEBMALYA <saudebmalya49@gmail.com>
2026-03-03 20:09:43 +0530

    aaadd if statmnt improve program

---
+++ if.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main ()
+{
+    int number ;
+    printf("mark: ");
+    scanf("%d",&number );
+    if (30<number<45)
+    {
+        printf("avarage ");
+
+    }
+    else if (number>45)
+    {
+        printf("good");
+    }
+    else
+    {
+        printf(" bokachuda");
+    }
+    
+    return 0;
+    
+}
